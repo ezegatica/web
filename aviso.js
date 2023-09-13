b15cc660b21b5bb04357c1102b9f96a784f5c287
Eze Gatica <git@ezegatica.com>
2023-09-12 22:10:42 -0300

    fix: Arreglando link a linkedin

--- aviso.js
+++ aviso.js
@@ -1,3 +1,3 @@
 console.log(`\n%cEze Gatica 🚀`, 'color:#00ff00; background:#0b1021; font-size:1.5rem; padding:0.15rem 0.25rem; margin: 1rem auto; font-family: Rockwell; border: 2px solid #00ff00; border-radius: 4px;font-weight: bold; text-shadow: 1px 1px 1px #00af87bf;');
 console.log("%c Hola 👋! Veo que te interesa como hice mi sitio. Si sos un reclutador y te gusta lo que hago, no dudes en contactarme por LinkedIn", 'font-size: 12px;font-family: monospace;background: white;display: inline-block;color: black;padding: 8px 19px;border: 1px dashed;');
-console.log("%c https://linkedin.com/ln/ezeg", 'font-size: 12px;font-family: monospace;background: black;display: inline-block;color: white;padding: 8px 19px;border: 1px dashed; text-align: center; width: 100%;');
+console.log("%c https://linkedin.com/in/ezeg", 'font-size: 12px;font-family: monospace;background: black;display: inline-block;color: white;padding: 8px 19px;border: 1px dashed; text-align: center; width: 100%;');
