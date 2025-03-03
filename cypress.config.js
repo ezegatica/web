3a5ad356c8becc1c63528bea5f25d9d3d6a9d0e3
Eze Gatica <git@ezegatica.com>
2025-03-03 14:23:13 -0300

    tests: Cypress corriendo :D

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    specPattern: 'assets/scripts/patentes/patentes.cy.js',
+    supportFile: false,
+    baseUrl: 'http://localhost:5500'
+  },
+});
